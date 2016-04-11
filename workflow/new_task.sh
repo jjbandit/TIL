82390aa72353c8543f65c482a5ceb2013249a8fd
Jesse Hughes <jhughes@excelsystems.com>
2016-04-11 10:21:22 -0700

    Add some parameter validation and help text to workflow/new_task.sh

--- workflow/new_task.sh
+++ workflow/new_task.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
-[ $# -eq 0 ] && echo "Please pass me a task number, exiting." && exit 1
+[ $# -eq 0 ] && echo "Please pass me a task title, exiting." && exit 1
+
+if [ $1 = "-h" ]; then 
+	echo "  -----------------------------------------------------"
+	echo ""
+	echo "  Usage: new_task.sh <Title> [Description] [Parent ID]"
+	echo ""
+	echo "    Required: Arg 1 specifies the task title."
+	echo ""
+	echo "    Optional: Arg 2 specifies the task description."
+	echo ""
+	echo "    Optional: Arg 3 specifies what task to use as the new tasks parent."
+	echo ""
+	echo "  -----------------------------------------------------"
+
+	exit 0
+fi
 
 isGitRepo=$(git rev-parse --is-inside-work-tree > /dev/null 2>&1)
-
 [ $? -ne 0 ] && echo "Not inside a git repo, exiting." && exit 1
 
+TASK_ID=$(fs_new_task.sh "$@" )
 
-TASK=$1
-
-powershell svn_branch $TASK
-~/til/git_svn/git_svn_track_remote_svn_branch.sh $TASK
+powershell svn_branch "$TASK_ID"
+~/til/git_svn/git_svn_track_remote_svn_branch.sh "$TASK_ID"
