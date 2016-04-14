423cf237944217f5d886506684161d08c446ce9b
Jesse Hughes <jhughes@excelsystems.com>
2016-04-14 16:12:57 -0700

    Do some error rewriting with sed.

--- workflow/build.sh
+++ workflow/build.sh
@@ -2,5 +2,9 @@
 
 # By default just build Websmart
 if [ $# -eq 0 ]; then
-	devenv.com WebSmartAll.sln /build
+	devenv.com "UnitTester/UnitTester.sln" /build  | \
+	sed 's/\\/\//g' | \
+	sed 's/[0-9]>\s*//g' | \
+	sed 's/c:\/users\/jhughes/\n\n~/g' | \
+	sed -e "s/error C[0-9]\+:\s*/\\n/"
 fi
