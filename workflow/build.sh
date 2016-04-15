bcd2f76b5fafecc0658e0d6a9475e76f04910b76
Jesse Hughes <jhughes@excelsystems.com>
2016-04-15 16:00:51 -0700

    Fix output not getting written to console in build.sh

--- workflow/build.sh
+++ workflow/build.sh
@@ -3,6 +3,7 @@
 # By default just build Websmart
 if [ $# -eq 0 ]; then
 	devenv.com "UnitTester/UnitTester.sln" /build  | \
+	tee /dev/tty | \
 	sed 's/\\/\//g' | \
 	sed 's/[0-9]>\s*//g' | \
 	sed 's/c:\/users\/jhughes/\n\n~/g' | \
