0b2264dcfee04af8c5421befa66ffdd1131b349d
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-05-04 14:36:04 -0700

    Add script to build the cscope database

---
+++ cscope/cscopeDB.sh
@@ -0,0 +1,6 @@
+# CScope can take an input of files from a list, so let's build that list first
+# wrapped in double quotes to handle spaces in filenames.
+
+find . -type f -print | grep -E '\.(c(pp)?|h)$' | sed 's/.*/"&"/' > cscope.list
+
+cscope -i cscope.list
