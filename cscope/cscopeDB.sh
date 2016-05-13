f8b4023479c5b2b092a12a3e4713a7b63dbf7e07
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-05-13 14:12:32 -0700

    Add check that'll build cscope.list to cscopeDB.sh

--- cscope/cscopeDB.sh
+++ cscope/cscopeDB.sh
@@ -1,6 +1,10 @@
 # CScope can take an input of files from a list, so we're going to build a list
 # of all source files (wrapped in quotes) then build our database from that.
 
+if [ ! -f cscope.list ]; then
+
 cpp_and_headers.sh > cscope.list
+ 
+fi
 
 cscope -b -i cscope.list
