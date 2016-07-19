f364a3eb3895f6341f102333ca266c40f536fb34
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-07-19 10:19:49 -0700

    Fix whitespace issue once and for all in svn_rever_all

--- svn/svn_revert_all.sh
+++ svn/svn_revert_all.sh
@@ -2,4 +2,4 @@
 
 # Svn doesn't have an equivalent of `git reset --hard` that works quickly, so this does it.
 
-svn st -q | awk '{print $NF}' | ~/til/sed/wrap_lines_in_quotes.sh | xargs svn revert
+svn st -q | sed -r -e 's/^.{8}//' | ~/til/sed/wrap_lines_in_quotes.sh | xargs svn revert
