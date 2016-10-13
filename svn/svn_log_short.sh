ca14dd1b8c91bd4673559d014041d67f15a89159
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-10-13 10:08:22 -0700

    Add author name to sls output

--- svn/svn_log_short.sh
+++ svn/svn_log_short.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 
-LOG=$(svn log --stop-on-copy | \
-	grep -A 2 "^r[0-9]\+" | \
-	sed -n '1~2p' | \
-	cut -d"|" -f 1 | \
-	sed 'N;s/\n/- /'
+LOG=$(svn log --stop-on-copy -l 40 \
+	| grep -A 2 "^r[0-9]\+" \
+	| sed -n '1~2p' \
 )
 
-echo "$LOG" | \
-	sed -e 's/^r[0-9]\+/'$(echo -e "\033[33m")'\0'$(echo -e "\033[37m")'/' | \
-	sed -e 's/r/ /'
+REV=$(echo "$LOG" \
+	| cut -d"|" -f 1 \
+	| sed -n '1~2p' \
+	| sed -e 's/.*/ '$(echo -e "\033[33m")'\0'$(echo -e "\033[37m")'/'
+)
+
+USER=$(echo "$LOG" \
+	| cut -d"|" -f 2 \
+	| sed -n '1~2p' \
+	| sed -e 's/.*/ '$(echo -e "\033[32m")'\0'$(echo -e "\033[37m")'	/'
+)
+
+MESSAGE=$(echo "$LOG" \
+	| sed -n '0~2p'
+)
+
+echo -e "$REV"      > .rev
+echo -e "$USER"     > .user
+echo -e "$MESSAGE"  > .message
+
+paste -d " " .rev .user .message
