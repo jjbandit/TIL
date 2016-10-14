ce59531394cdc30d2c7a3b736770feb917c80088
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-10-14 13:46:46 -0700

    Trim whitespace on revision and username

--- svn/svn_log_short.sh
+++ svn/svn_log_short.sh
@@ -8,12 +8,16 @@ LOG=$(svn log --stop-on-copy -l 40 \
 REV=$(echo "$LOG" \
 	| cut -d"|" -f 1 \
 	| sed -n '1~2p' \
+	| sed 's/^\s*//g' \
+	| sed 's/\s*$//g' \
 	| sed -e 's/.*/ '$(echo -e "\033[33m")'\0'$(echo -e "\033[37m")'/'
 )
 
 USER=$(echo "$LOG" \
 	| cut -d"|" -f 2 \
 	| sed -n '1~2p' \
+	| sed 's/^\s*//g' \
+	| sed 's/\s*$//g' \
 	| sed -e 's/.*/ '$(echo -e "\033[32m")'\0'$(echo -e "\033[37m")'	/'
 )
 
@@ -25,4 +29,5 @@ echo -e "$REV"      > .rev
 echo -e "$USER"     > .user
 echo -e "$MESSAGE"  > .message
 
-paste -d " " .rev .user .message
+paste -d " " .rev .user .message \
+	| sed 's/\s\{2,\}/ | /g'
