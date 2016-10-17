41d520c0a8c93e1df02dee60e3405bb06ff66227
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-10-17 11:03:22 -0700

    Fix url-encoded [ and ] characters

--- flyspray/fs_task_list.sh
+++ flyspray/fs_task_list.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
 
-TASK_HTML=$(http --follow GET http://flyspray.excelsystems.com/flyspray \
-	do==index project==2 type%5B0%5D==2 type%5B1%5D==1 type%5B2%5D==21 type%5B3%5D==20 \
-	dev==jhughes status%5B0%5D==2 status%5B1%5D==3 status%5B2%5D==7 status%5B3%5D==4 \
-	status%5B4%5D==5 status%5B5%5D==6 status%5B6%5D==12 status%5B7%5D==13 \
-	status%5B8%5D==14 status%5B9%5D==15 status%5B10%5D==16 status%5B11%5D==9
+TASK_HTML=$(http --follow GET http://flyspray.excelsystems.com/flyspray/ \
+	do==index \
+	project==2 \
+	type[0]==2 \
+	type[1]==1 \
+	type[2]==21 \
+	type[3]==20 \
+	sev[0]== \
+	pri[0]== \
+	due[0]== \
+	dev==jhughes \
+	cat[0]== \
+	status[0]==2 \
+	status[1]==3 \
+	status[2]==7 \
+	status[3]==4 \
+	status[4]==5 \
+	status[5]==6 \
+	status[6]==12 \
+	status[7]==13 \
+	status[8]==14 \
+	status[9]==15 \
+	status[10]==16 \
+	status[11]==9 \
+	percent[0]== \
+	search_in_details==1 \
+	reported[0]==
 )
-
 
 TASK_LIST=$( echo "$TASK_HTML" \
 	| grep -Po "task_summary.*?/td>" \
