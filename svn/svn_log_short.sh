f068e00e3be741ba4cc8a56c82c39b8c5cf4d350
Jesse Hughes <revelstokesnowboard@gmail.com>
2016-10-17 09:06:43 -0700

    Add --nocolor option to sls

--- svn/svn_log_short.sh
+++ svn/svn_log_short.sh
@@ -9,25 +9,33 @@ REV=$(echo "$LOG" \
 	| cut -d"|" -f 1 \
 	| sed -n '1~2p' \
 	| sed 's/^\s*//g' \
-	| sed 's/\s*$//g' \
-	| sed -e 's/.*/ '$(echo -e "\033[33m")'\0'$(echo -e "\033[37m")'/'
+	| sed 's/\s*$//g'
 )
 
 USER=$(echo "$LOG" \
 	| cut -d"|" -f 2 \
 	| sed -n '1~2p' \
 	| sed 's/^\s*//g' \
-	| sed 's/\s*$//g' \
-	| sed -e 's/.*/ '$(echo -e "\033[32m")'\0'$(echo -e "\033[37m")'	/'
+	| sed 's/\s*$//g'
 )
 
 MESSAGE=$(echo "$LOG" \
 	| sed -n '0~2p'
 )
 
+if [ "$1" != "--nocolor" ]; then
+	REV=$( echo "$REV" \
+		| sed -e 's/.*/ '$(echo -e "\033[33m")'\0'$(echo -e "\033[37m")'/'
+	)
+
+	USER=$(echo "$USER" \
+	| sed -e 's/.*/ '$(echo -e "\033[32m")'\0'$(echo -e "\033[37m")'	/'
+	)
+fi
+
 echo -e "$REV"      > .rev
 echo -e "$USER"     > .user
 echo -e "$MESSAGE"  > .message
 
 paste -d " " .rev .user .message \
-	| sed 's/\s\{2,\}/ | /g'
+	| sed 's/\s\{2,\}/  |  /g'
