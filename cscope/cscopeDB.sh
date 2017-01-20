71dbeb438bb11a1c360a2912f32663512425e72b
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-01-20 12:48:42 -0800

    Add ctags building to cscopeDB

--- cscope/cscopeDB.sh
+++ cscope/cscopeDB.sh
@@ -1,9 +1,17 @@
 # CScope can take an input of files from a list, so we're going to build a list
 # of all source files (wrapped in quotes) then build our database from that.
 
-if [ ! -f cscope.list ]; then
-	cpp_and_headers.sh > cscope.list
-fi
+[ -f cscope.out ] && echo "Removing old cscope.out" && rm cscope.out
+[ -f tags ] && echo "Removing old tags" && rm tags
 
-cscope -b -i cscope.list
-rm cscope.list
+cpp_and_headers.sh > file_list
+
+echo "Starting index on $(cat file_list | wc -l) Files"
+
+echo "Building ctags DB"
+find . -type f -name \*.cpp -o -name \*.h | ctags --language-force=C++ -L -
+
+echo "Building cscope DB"
+cscope -b -i file_list
+
+rm file_list
