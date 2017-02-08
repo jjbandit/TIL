71bdde986242c58ed8555b59193e6586e7d8d65e
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-02-08 11:25:01 -0800

    Add nexus/createEnv.sh

---
+++ nexus/createEnv.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# This emulates the weird-ass createEnv stuff in one layer instead of 4
+
+[ $# -eq 0 ] && echo "Please provide a branch number, exiting." && exit 1
+
+DOWNLOAD_TXT=$(ack.cmd -gi download.txt)
+WS_SVN_CONFIG_TXT="Development PDWs/ws_svn_config.txt"
+
+echo "$DOWNLOAD_TXT"
+echo "$WS_SVN_CONFIG_TXT"
+
+function ReplaceNexusBranch()
+{
+	FILE="$1"
+	cat "$FILE" | perl -pe 's/NX[0-9]{5}/NX'"$BRANCH_NUMBER"'/g' > tmp && mv tmp "$FILE"
+	cat "$FILE" | perl -pe 's/NX_TRK/NX12345/g' > tmp && mv tmp "$FILE"
+}
+
+
+BRANCH_NUMBER="$1"
+WEBPATH="8099/NX$BRANCH_NUMBER/nexus/"
+SOURCELIB="NX$BRANCH_NUMBER"
+PROJECT="Nexus FS $BRANCH_NUMBER"
+SET="*DEV"
+
+if [ "$BRANCH_NUMBER" == "trunk" ]; then
+	BRANCH_NUMBER="_TRK"
+	WEBPATH="8100/nexus/"
+	SOURCELIB="NX_TRK_SRC"
+	PROJECT="Nexus Trunk"
+	SET="*PROD"
+fi
+
+# Replace ws_svn_config.txt with the new branch info
+echo "SVN='*'
+{
+        OBJLIBRARY='NX$BRANCH_NUMBER';
+        SOURCEFILE='QPDWSRC';
+        SOURCELIBR='$SOURCELIB';
+        WEBPATH='http://dev.excelsystems.com:$WEBPATH';
+        TARGETPATH='http://dev.excelsystems.com:$WEBPATH';
+        COMPLIBLOBJ='*OBJLIBNAM';
+        PROJECT='$PROJECT';
+        SET='$SET';
+}" > "$WS_SVN_CONFIG_TXT" && unix2dos.exe "$WS_SVN_CONFIG_TXT"
+
+# Update any download.txt files we might encounter
+echo "$DOWNLOAD_TXT" |  while read -r file; do
+	ReplaceNexusBranch "$file"
+done
