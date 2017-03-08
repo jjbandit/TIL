bd04ab9eb3b24d18fb3931ed8a584560e28490ca
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-03-08 10:10:28 -0800

    Change filename cscope.sh operates on internally

--- cscope/cscopeDB.sh
+++ cscope/cscopeDB.sh
@@ -4,14 +4,16 @@
 [ -f cscope.out ] && echo "Removing old cscope.out" && rm cscope.out
 [ -f tags ] && echo "Removing old tags" && rm tags
 
-cpp_and_headers.sh > file_list
+cpp_and_headers.sh > cscope.files
 
-echo "Starting index on $(cat file_list | wc -l) Files"
+echo "Starting index on $(cat cscope.files | wc -l) Files"
 
 echo "Building ctags DB"
 find . -type f -name \*.cpp -o -name \*.h | ctags --language-force=C++ -L -
 
 echo "Building cscope DB"
-cscope -b -i file_list
+cscope -b -i cscope.files
 
-rm file_list
+cat cscope.files
+
+rm cscope.files
