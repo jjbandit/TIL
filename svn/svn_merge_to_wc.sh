8eb72cea31b3239d92b0ac5c19fc7fcb956136fc
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-06-26 09:49:24 -0700

    Better error message on svn_merge_to_wc

--- svn/svn_merge_to_wc.sh
+++ svn/svn_merge_to_wc.sh
@@ -7,18 +7,17 @@ if [[ $# -eq 0 ]]; then
 	exit 1
 fi
 
-project=$(svn info | grep --color=never Relative | grep -o "\^/\w*\?/" | sed 's/[/^]//g')
+project=$(svn info | grep --color=never Relative | grep -o "\^/.*\?/" )
 
-branch_path=^/$project/branches/FS\ $1
+branch_path="$project/branches/FS%20$1"
 
 from_rev=$(svn log --stop-on-copy "$branch_path" | grep -P "^r\d\d+" | cut -d" " -f 1 | tail -n 1 | sed 's/r//')
 
-echo "From: $branch_path@$from_rev"
-
 wc_head=$(svn info | grep "Revision" | cut -d" " -f2)
 
-echo "Working Copy is at: $wc_head"
+echo "Project: $project, From : $from_rev, To: $wc_head"
 
-svn merge --ignore-ancestry -r$from_rev:$wc_head "$branch_path"
+svn merge --ignore-ancestry -r"$from_rev:$wc_head" "$branch_path"
+[ $? -ne 0 ] && echo "Error stating branch" && exit 1
 
 svn_commit.sh "$1"
